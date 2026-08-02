f28b65b2b5db9b6aed7fa2265498a1db230051ed
Alex Moemeka <alexmoemeka@Alexs-MacBook-Pro.local>
2026-08-02 16:57:32 -0400

    Version 1 completion of Student Grade Manager

--- main.py
+++ main.py
@@ -2,32 +2,41 @@ print("=====================")
 print("Student Grade Manager")
 print("=====================")
 
-teacher_name = input("What is your name: ")
+teacher_name = input("What is your name: ").title()
+running = True
+students = ["Alex Jr", "Elizabeth Moemeka", "Lebron James"]
 
 print()
 print(f"Hello {teacher_name}! Welcome to your very own Student Grade Manager!")
 print()
-print("1. Add student")
-print("2. View Students")
-print("3. Exit")
+while running:
+    print("1. Add student")
+    print("2. View Students")
+    print("3. Exit")
+    print()
 
-students = ["Alex Jr", "Elizabeth Moemeka", "Lebron James"]
+    option_num = int(input("What would you like to do? "))
 
-option_num = int(input("What would you like to do? "))
+    if option_num == 1:
+        added_student = input("Student name: ").title()
+        students.append(added_student)
+        students.sort()
+        
+        print(f"{added_student} has been added to your class!")
 
-if option_num == 1:
-    added_student = input("Student name:")
-    students.append(added_student)
-    print(f"{added_student} has been added to your class!")
-
-elif option_num == 2:
-    for student in students:
-        print(student)
+    elif option_num == 2:
+        print("----------------")
+        print(f"{teacher_name}'s Class List!")
+        print("----------------")
+        
+        for student in students:
+            print(student)
     
 
-elif option_num == 3:
-    print("You have successfully been logged out")
+    elif option_num == 3:
+        print("You have successfully been logged out")
+        running = False
 
-else:
-   print("Not an option. Restart program if you wish to try again")
+    else:
+        print("Invalid option. Please enter 1, 2, or 3")
 
